4cf009a5f6607e5e82033303e028f10ef3867fcd
darkj0k <rybushkin09@bk.ru>
2025-01-06 13:23:05 +0400

    Update: Add GUI

--- start.bat
+++
@@ -1,5 +0,0 @@
-python -m venv .venv
-.venv/Scripts/activate.bat
-python -m pip install -r requirements.txt
-cd C://"Program Files"/Google/Chrome/Application/
-chrome.exe --remote-debugging-port=9222
